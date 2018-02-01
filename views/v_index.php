9ca1dec97bb590fd03e72c550b0fb56aa12b0b1f
PEREIRA MENDES CRISTIANO <cristiano.prrmn@eduge.ch>
2018-02-01 15:10:14 +0100

    Ajout SQL

--- views/v_index.php
+++ views/v_index.php
@@ -30,32 +30,41 @@
             <!-- Partie droite -->
             <div class="col-md-9 col-xs-9" style="background-color: #2a2b2b; color: white; height: 800px;">
                 <div class="col-md-12 col-xs-12" style="background-color: #1b2021; color: black; border: 1px; height: 560px; margin-top: 20px;">
-                    <canvas style="border: 1px;"></canvas>
+                    <canvas id="game"></canvas>
                 </div>
 
                 <div class="col-md-12 col-xs-12" style="background-color: #1b2021; margin-top: 20px; height: 180px;">
                     <ul class="nav nav-pills">
-                        <li><a data-toggle="pill" href="#onePoint" style="color: white; text-decoration: none;" onmouseover="this.style.color = 'white'; this.style.background = '#419222';" onmouseout="this.style.color = 'white'; this.style.background = 'none'">One point bet</a></li>
-                        <li><a data-toggle="pill" href="#game" style="color: white; text-decoration: none;" onmouseover="this.style.color = 'white'; this.style.background = '#419222';" onmouseout="this.style.color = 'white'; this.style.background = 'none'">Game bet</a></li>
+                        <li><a data-toggle="pill" href="#onePointBet" style="color: white; text-decoration: none;" onmouseover="this.style.color = 'white'; this.style.background = '#419222';" onmouseout="this.style.color = 'white'; this.style.background = 'none'">One point bet</a></li>
+                        <li><a data-toggle="pill" href="#gameBet" style="color: white; text-decoration: none;" onmouseover="this.style.color = 'white'; this.style.background = '#419222';" onmouseout="this.style.color = 'white'; this.style.background = 'none'">Game bet</a></li>
                     </ul>
 
                     <div class="tab-content">
-                        <div id="onePoint" class="tab-pane fade in active">
+                        <div id="onePointBet" class="tab-pane fade in active">
                             <form method="post">
                                 <fieldset>
                                     <legend style="color:white;">One point bet</legend>
                                     <input id="submit" name="submitL" type="submit" class="btn btn-primary" value="Bet !" style="background-color: #970a08; border: none; float: left; width: 200px; height: 50px;">
-                                    <input type="number" style="text-align: center;">
+                                    <input type="number" style="text-align: center; color: black; margin-left: 25%;">
+                                    <label>Ciseaux</label>
+                                    <input type="radio" name="choice">
+                                    <label>Pierre</label>
+                                    <input type="radio" name="choice">
+                                    <label>Feuille</label>
+                                    <input type="radio" name="choice">
                                     <input id="submit" name="submitR" type="submit" class="btn btn-primary" value="Bet !" style="background-color: #419222; border: none; float: right; width:200px; height: 50px;">
                                 </fieldset>
                             </form>
                         </div>
-                        <div id="game" class="tab-pane fade">
-                            <form method="post" style="margin-top: 20px;">
-                                <input id="submit" name="submitL" type="submit" class="btn btn-primary" value="Bet !" style="background-color: #970a08; border: none; float: left; width: 200px; height: 50px;">
-
-                                <input id="submit" name="submitR" type="submit" class="btn btn-primary" value="Bet !" style="background-color: #419222; border: none; float: right; width:200px; height: 50px;">
-                            </form>
+                        <div id="gameBet" class="tab-pane fade">
+                          <form method="post">
+                              <fieldset>
+                                  <legend style="color:white;">Game bet</legend>
+                                  <input id="submit" name="submitL" type="submit" class="btn btn-primary" value="Bet !" style="background-color: #970a08; border: none; float: left; width: 200px; height: 50px;">
+                                  <input type="number" style="text-align: center;">
+                                  <input id="submit" name="submitR" type="submit" class="btn btn-primary" value="Bet !" style="background-color: #419222; border: none; float: right; width:200px; height: 50px;">
+                              </fieldset>
+                          </form>
                         </div>
                     </div>
                 </div>

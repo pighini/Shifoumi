30dc9aa32cb44690cb7d10e20cb8ca41e4db75ee
LilThib <thibaut.mchd@eduge.ch>
2018-02-01 16:42:43 +0100

    Fin de la journée 1(mouvement de main)

--- assets/js/scriptAnim.js
+++ assets/js/scriptAnim.js
@@ -1,14 +1,48 @@
-function myMove() {
-  var elem = document.getElementById("myAnimation");
-  var pos = 0;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
-    }
-  }
+
+
+function Anim(optionGauche, optionDroite) {
+  // Définition des variables globales
+  var canvas  = document.getElementById("game");
+  var ctx     = canvas.getContext("2d");
+
+  var directory = "assets/images/";
+  var type = ".png";
+
+  var MainGauche = new Image();
+  MainGauche.src = directory.concat(optionGauche.concat(type));
+
+
+  var MainDroite = new Image();
+  MainDroite.src = directory.concat(optionDroite.concat(type));
+
+  var MainMoveG = new Image();
+  MainMoveG.src = directory.concat("move".concat(type));
+
+  var MainMoveD = new Image();
+  MainMoveD.src = directory.concat("move".concat(type));
+
+   var id = setInterval(down, 10);
+   var cpt = 0;
+   var posY = 100;
+
+   function down() {
+   ctx.clearRect(20, posY, 50, 50);
+   ctx.clearRect(330, posY, 50, 50);
+
+   posY = posY + 1;
+
+   ctx.drawImage(MainMoveD, 20, posY);
+   ctx.drawImage(MainMoveG, 330, posY);
+
+   if (cpt >= 50) {
+     clearInterval(id);
+   }
+   else
+   {
+     cpt++;
+   }
+ }
+
+ var id = setInterval(frame, 10);
+
 }

925666aa6e48b2dc6d2c8fc1bf12db139c3f1b26
pighini <lucas.pghn@eduge.ch>
2018-03-08 15:01:25 +0100

    testN2

--- model/fonctions.php
+++ model/fonctions.php
@@ -2,27 +2,62 @@
 
 require_once 'dbConnection.php';
 
-function PseudoExist($pseudo){
-    $db = connectDb();
+function usernameAvailable($pseudo){
+    $db = MyPdo();
     $request = $db->prepare("SELECT * FROM users WHERE username = :username");
     $request->execute(array('username' => $pseudo));
 
     return ($request->rowCount() > 0);
 }
-function addUser($username, $email ,$pwd, $balance){
-    $db = connectDb();
-    $password = sha1($pwd);
-    $salt = sha1(rand(0,100));
-    $password = sha1($salt.$password);
+function addUser($username, $emailUser ,$pwd, $balance){
+    $db = MyPdo();
+    $userN = $username;
+    $email = $emailUser;
+    $password = $pwd;
+    $balance = $balanceUser;
     $request = $db->prepare("INSERT INTO users(`username`, `email`, `password`,  `balance`)
-            VALUES(:FistName, :LastName, :Pwd, :Pseudo, :salt, :email, :sms)");
+            VALUES(:userN , :email, :password, :balance)");
     $request->execute(array(
-        'username' => uname,
-        'email' => $password,
+        'username' => $userN,
         'password' => $password,
-        'balance' => $salt,
-
+        'email' => $email,
+        'balance' => $balanceUser
    ));
 }
 
+function emailExist($email){
+  $db = MyPdo();
+  $request = $db->prepare("SELECT * FROM users WHERE email = :email");
+  $request->execute(array('email' => $email));
+
+  return ($request->rowCount() > 0);
+}
+
+function checkUserAuthentification($pseudo, $pwd){
+    $db = MyPdo();
+    $password = $pwd;
+    $sql = "SELECT count(*) FROM users WHERE username = :username AND password = :password ";
+    $request = $db->prepare($sql);
+    $request->execute(
+      array(
+        'pseudo' => $pseudo,
+        'password' => $password
+    ));
+
+    return $request->fetch()[0]!=0;
+}
+
+function newGame()
+{
+
+
+}
+function chooseShape()
+{
+
+}
+function makeABet()
+{
+
+}
 ?>

c73ea86c7f105aede9cb73be9f4f1cf9b0960b25
pighini <lucas.pghn@eduge.ch>
2018-03-08 16:12:51 +0100

    newChange functions

--- model/fonctions.php
+++ model/fonctions.php
@@ -2,70 +2,14 @@
 
 require_once 'dbConnection.php';
 
-function usernameAvailable($username){
-    $db = myPdo();
+function usernameAvailable($pseudo){
+    $db = MyPdo();
     $request = $db->prepare("SELECT * FROM users WHERE username = :username");
-    $request->execute(array('username' => $username));
+    $request->execute(array('username' => $pseudo));
 
     return ($request->rowCount() > 0);
 }
 function addUser($username, $emailUser ,$pwd, $balance){
-    $db = myPdo();
-    $userN = $username;
-    $email = $emailUser;
-    $password = sha1($pwd);
-    $balance = $balanceUser;
-    $request = $db->prepare("INSERT INTO users(`username`, `email`, `password`,  `balance`)
-            VALUES(:userN , :email, :password, :balance)");
-    $request->execute(array(
-        'username' => $userN,
-        'password' => $password,
-        'email' => $email,
-        'balance' => $balanceUser
-   ));
-}
-
-function emailExist($email){
-  $db = myPdo();
-  $request = $db->prepare("SELECT * FROM users WHERE email = :email");
-  $request->execute(array('email' => $email));
-
-  return ($request->rowCount() > 0);
-}
-
-function checkUserAuthentification($username, $pwd){
-    $db = myPdo();
-    $password = sha1($pwd);
-    $sql = "SELECT count(*) FROM users WHERE username = :username AND password = :password";
-    $request = $db->prepare($sql);
-    $request->execute(
-      array(
-        'username' => $username,
-        'password' => $password
-    ));
-
-    return $request->fetch()[0] != 0;
-}
-
-function getBestUsers() {    
-    $db = connectDb();
-    $sql = "SELECT `username`, `balance` FROM `users` ORDER BY `balance` DESC LIMIT 10";
-    $request = $db->query($sql);
-    return $request;
-}
-
-function newGame()
-{
-
-
-}
-function chooseShape()
-{
-
-}
-function makeABet()
-{
-
     $db = MyPdo();
     $userN = $username;
     $email = $emailUser;
@@ -78,6 +22,54 @@ function makeABet()
         'password' => $password,
         'email' => $email,
         'balance' => $balanceUser
+   ));
+}
+
+function emailExist($email){
+  $db = MyPdo();
+  $request = $db->prepare("SELECT * FROM users WHERE email = :email");
+  $request->execute(array('email' => $email));
+
+  return ($request->rowCount() > 0);
+}
+
+function checkUserAuthentification($pseudo, $pwd){
+    $db = MyPdo();
+    $password = $pwd;
+    $sql = "SELECT count(*) FROM users WHERE username = :username AND password = :password ";
+    $request = $db->prepare($sql);
+    $request->execute(
+      array(
+        'pseudo' => $pseudo,
+        'password' => $password
+    ));
+
+    return $request->fetch()[0]!=0;
+}
+
+function newGame()
+{
+
+
+}
+function chooseShape()
+{
+
+}
+function makeABet($amount , $id)
+{
+
+    $db = MyPdo();
+    $userN = $username;
+    $email = $emailUser;
+    $password = $pwd;
+    $balance = $balanceUser;
+    $request = $db->prepare("INSERT INTO bets(`amount`, `idUser`)
+            VALUES(:amount , :email)");
+    $request->execute(array(
+        'amount' => $amount,
+        'idUser' => $id
         ));
+
   }
 ?>

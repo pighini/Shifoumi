2e1932f28a823afade50e27e70a991fe71eb84b3
pighini <lucas.pghn@eduge.ch>
2018-03-15 13:56:34 +0100

    Beggining of the day -test

--- model/fonctions.php
+++ model/fonctions.php
@@ -48,7 +48,7 @@ function checkUserAuthentification($pseudo, $pwd){
 }
 
 function getBestUsers() {
-    $db = myPdo();
+    $db = MyPdo();
     $sql = "SELECT `username`, `balance` FROM `users` ORDER BY `balance` DESC LIMIT 10";
     $request = $db->query($sql);
     return $request;
@@ -59,11 +59,15 @@ function newGame()
 
 
 }
-function chooseShape()
+function makeBet($choice, $amount)
 {
 
 }
-function makeABet($amount , $id)
+function hasWon()
+{
+
+}
+function insertBet($amount , $id)
 {
 
     $db = MyPdo();
@@ -80,8 +84,6 @@ function makeABet($amount , $id)
 
   }
 
-  function haveWonPoint($choice, $shape,)
-  {
 
-  }
+
 ?>
